a606616c8b19223fa90e5c1c1590555548f09e39
mfauzanalg <mfauzanalg@gmail.com>
2020-04-06 15:33:59 +0700

    nyoba fxml tapi blm bisa hmm

--- src/main/java/com/avatarduel/AvatarDuel.java
+++ src/main/java/com/avatarduel/AvatarDuel.java
@@ -6,7 +6,9 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -30,26 +32,13 @@ public class AvatarDuel extends Application {
 
   @Override
   public void start(Stage stage) {
-    Text text = new Text();
-    text.setText("Loading...");
-    text.setX(50);
-    text.setY(50);
 
-    Group root = new Group();
-    root.getChildren().add(text);
-
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
     Scene scene = new Scene(root, 1280, 720);
 
     stage.setTitle("Avatar Duel");
     stage.setScene(scene);
     stage.show();
-
-    try {
-      this.loadCards();
-      text.setText("Avatar Duel!");
-    } catch (Exception e) {
-      text.setText("Failed to load cards: " + e);
-    }
   }
 
   public static void main(String[] args) {
---
+++ src/main/java/com/avatarduel/Controller.java
@@ -0,0 +1,4 @@
+package com.avatarduel;
+
+public class Controller {
+}
