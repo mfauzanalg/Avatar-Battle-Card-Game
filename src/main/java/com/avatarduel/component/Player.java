fe6b751e3c8d93a30de09eba22eeff34acdcecb0
IrfaanDzakiy <dzakiyirfaan@gmail.com>
2020-04-07 15:32:15 +0700

    update player

--- src/main/java/com/avatarduel/component/Player.java
+++ src/main/java/com/avatarduel/component/Player.java
@@ -1,13 +1,20 @@
 package com.avatarduel.component;
+import java.util.*;
 
-public class Player {
+public class Player{
     private String name;
     private int health;
+    private Stack<Card> deck;
+    private List<IHandCard> hand;
+    private List<ISummoned> board;
 
-    public Player()
+    public Player(String name, int health)
     {
-        this.name = "Fauzan";
-        this.health = 70000;
+        this.name = name;
+        this.health = health;
+        this.deck = new Stack<Card>();
+        this.hand = new List<IHandCard>();
+        this.board = new List<ISummoned>();
     }
 
     public void setName(String name)
@@ -18,4 +25,44 @@ public class Player {
     public String getName() {
         return this.name;
     }
+
+    public void setHealth(int health){
+        this.health = health;
+	}
+
+    public int getHealth(){
+        return this.health;
+	}
+
+    public void draw(){
+        //pop card dari deck, add card ke hand;
+        System.out.println("belum diimplementasi");
+	}
+
+    public void play(){
+        //play kartu di tangan yang dipilih Player
+        //cara tahu kartu yang dipilih player gimana???
+        System.out.println("belum diimplementasi");
+        //play card from hand
+        //remove card from hand
+        //add card to board
+	}
+
+    public void rotate(){
+        //rotate kartu di board yang dipilih Player
+        //cara tahu kartu yang dipilih player gimana???
+        System.out.println("belum diimplementasi");
+	}
+
+    public void attack(){
+        //menyerang player/character musuh menggunakan character di board
+        //cara tahu kartu yang dipilih player gimana???
+        System.out.println("belum diimplementasi");
+	}
+
+    public void destroy(){
+        //membuang kartu skill di board;
+        //cara tahu kartu yang dipilih player gimana???
+        System.out.println("belum diimplementasi");
+	}
 }
