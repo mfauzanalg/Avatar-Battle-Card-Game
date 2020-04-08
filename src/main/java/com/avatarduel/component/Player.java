05bb797934b39764af44efbff90f92302ffc3d92
IrfaanDzakiy <dzakiyirfaan@gmail.com>
2020-04-08 13:38:49 +0700

    implementasi land n power di player n card

--- src/main/java/com/avatarduel/component/Player.java
+++ src/main/java/com/avatarduel/component/Player.java
@@ -1,5 +1,6 @@
 package com.avatarduel.component;
 import java.util.*;
+import com.avatarduel.model;
 
 public class Player{
     private String name;
@@ -8,6 +9,10 @@ public class Player{
     private List<IHandCard> hand; //Ini harus IHandCard karna method play adanya di IHandCard
     private List<BoardCard> board; // Ini juga harus BoardCard biar bisa pake dekorator
     private List<SkillBoardCard> skillBoard; // Kalo perlu Cardnya, pake method getCardInstance() ya
+    private int maxWater, currentWater,
+                maxEarth, currentEarth,
+                maxFire, currentFire,
+                maxAir, currentAir;
 
     public Player(String name, int health)
     {
@@ -17,6 +22,10 @@ public class Player{
         this.hand = new ArrayList<IHandCard>();
         this.board = new ArrayList<BoardCard>();
         this.skillBoard = new ArrayList<SkillBoardCard>();
+        this.maxWater = 0; this.currentWater = 0;
+        this.maxEarth = 0; this.currentEarth = 0;
+        this.maxFire = 0; this.currentFire = 0;
+        this.maxAir = 0; this.currentAir = 0;
     }
 
     public void setName(String name)
@@ -34,6 +43,68 @@ public class Player{
 
     public int getHealth(){
         return this.health;
+	}
+
+    public void setMaxElement(String element, int val ){
+        switch(element){
+            case ("FIRE"):
+                this.maxFire = val;
+            case ("WATER"):
+                this.maxWater = val;
+            case ("EARTH"):
+                this.maxEarth = val;
+            case ("AIR"):
+                this.maxAir = val;
+            default:
+                System.out.println("No Match");
+		}
+	}
+
+    public int getMaxElement(String element){
+        switch(element){
+            case ("FIRE"):
+                return this.maxFire;
+            case ("WATER"):
+                return this.maxWater;
+            case ("EARTH"):
+                return this.maxEarth;
+            case ("AIR"):
+                return this.maxAir;
+            default:
+                System.out.println("No Match");
+                return;
+		}
+	}
+
+    public void setCurrentElement(String element, int val ){
+        switch(element){
+            case ("FIRE"):
+                this.currentFire = val;
+            case ("WATER"):
+                this.currentWater = val;
+            case ("EARTH"):
+                this.currentEarth = val;
+            case ("AIR"):
+                this.currentAir = val;
+            default:
+                System.out.println("No Match");
+		}
+	}
+
+    public int getCurrentElement(String element){
+        switch(element){
+            case ("FIRE"):
+                return this.currentFire;
+            case ("WATER"):
+                return this.currentWater;
+            case ("EARTH"):
+                return this.currentEarth;
+            case ("AIR"):
+                return this.currentAir;
+            default:
+                System.out.println("No Match");
+                return;
+		}
 	}
 
     public void draw(){
