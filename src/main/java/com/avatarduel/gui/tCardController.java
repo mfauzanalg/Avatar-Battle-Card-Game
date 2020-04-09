818eb22ba6acb54416a6725bb001f9f25b6da8c3
mfauzanalg <mfauzanalg@gmail.com>
2020-04-09 13:50:09 +0700

    load kartu tipe beda2

--- src/main/java/com/avatarduel/gui/tCardController.java
+++ src/main/java/com/avatarduel/gui/tCardController.java
@@ -1,7 +1,7 @@
 package com.avatarduel.gui;
 
 import com.avatarduel.AvatarDuel;
-import com.avatarduel.component.Card;
+import com.avatarduel.component.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -19,14 +19,77 @@ public class tCardController {
     @FXML private Text elmt;
 
 
-    public void loadCard(Card card){
+
+    public void loadLandCard(Card card){
         File file = new File (card.getImagePath());
         Image image = new Image(file.toURI().toString());
         pict.setImage(image);
         elmt.setText(card.getElement());
         name.setText(card.getName());
-        attrib.setText("apa ya");
+        attrib.setText("this is " + card.getElement() + " Land Card");
         System.out.println(card.getImagePath());
+    }
+
+    public void loadCharCard(CharacterCard card){
+        File file = new File (card.getImagePath());
+        Image image = new Image(file.toURI().toString());
+        pict.setImage(image);
+        elmt.setText(card.getElement());
+        name.setText(card.getName());
+        attrib.setText("Attack " + card.getAttack() + " Def : " + card.getDefense() + " Pow " + card.getPower());
+        System.out.println(card.getImagePath());
+    }
+
+    public void loadSkillCard(SkillCard card){
+        File file = new File (card.getImagePath());
+        Image image = new Image(file.toURI().toString());
+        pict.setImage(image);
+
+        String eff = card.getEffect();
+        if (eff.equals("aura")){
+            this.loadAuraCard((AuraSkillCard) card);
+        }
+        else if (eff.equals("destroy")){
+            this.loadDestroyCard((DestroySkillCard) card);
+        }
+        else{
+            this.loadPowerUpCard((PowerUpSkillCard) card);
+        }
+    }
+
+    public void loadAuraCard(AuraSkillCard card){
+        elmt.setText(card.getElement());
+        name.setText(card.getName());
+        attrib.setText("Attack " + card.getAttack() + " Def : " + card.getDefense() + " Pow " + card.getPower());
+        System.out.println(card.getImagePath());
+    }
+
+    public void loadDestroyCard(DestroySkillCard card){
+        elmt.setText(card.getElement());
+        name.setText(card.getName());
+        attrib.setText("This is Destroy Card");
+        System.out.println(card.getImagePath());
+    }
+
+    public void loadPowerUpCard(PowerUpSkillCard card){
+        elmt.setText(card.getElement());
+        name.setText(card.getName());
+        attrib.setText("This is Destroy Card");
+        System.out.println(card.getImagePath());
+    }
+
+
+    public void loadCard(Card card){
+        String type = card.getType();
+        if (type.equals("land")){
+            this.loadLandCard(card);
+        }
+        else if (type.equals("character")){
+            this.loadCharCard((CharacterCard) card);
+        }
+        else{
+            this.loadSkillCard((SkillCard) card);
+        }
     }
 
 
