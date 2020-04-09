5db26845ac9667c410de7fa178d9c05f2b92f96d
mfauzanalg <mfauzanalg@gmail.com>
2020-04-09 08:35:11 +0700

    Update layoutController.java

--- src/main/java/com/avatarduel/gui/layoutController.java
+++ src/main/java/com/avatarduel/gui/layoutController.java
@@ -23,10 +23,57 @@ public class layoutController implements Initializable{
     @FXML private tCardController P1H2Controller;
     @FXML private tCardController P1H3Controller;
     @FXML private tCardController P1H4Controller;
+    @FXML private tCardController P1H5Controller;
+    @FXML private tCardController P1H6Controller;
+    @FXML private tCardController P1H7Controller;
+    @FXML private tCardController P1H8Controller;
+    @FXML private tCardController P1H9Controller;
 
+    @FXML private tCardController P1A1Controller;
+    @FXML private tCardController P1A2Controller;
+    @FXML private tCardController P1A3Controller;
+    @FXML private tCardController P1A4Controller;
+    @FXML private tCardController P1A5Controller;
+    @FXML private tCardController P1A6Controller;
+    @FXML private tCardController P1A7Controller;
+    @FXML private tCardController P1A8Controller;
 
+    @FXML private tCardController P1S1Controller;
+    @FXML private tCardController P1S2Controller;
+    @FXML private tCardController P1S3Controller;
+    @FXML private tCardController P1S4Controller;
+    @FXML private tCardController P1S5Controller;
+    @FXML private tCardController P1S6Controller;
+    @FXML private tCardController P1S7Controller;
+    @FXML private tCardController P1S8Controller;
 
+    @FXML private tCardController P2H1Controller;
+    @FXML private tCardController P2H2Controller;
+    @FXML private tCardController P2H3Controller;
+    @FXML private tCardController P2H4Controller;
+    @FXML private tCardController P2H5Controller;
+    @FXML private tCardController P2H6Controller;
+    @FXML private tCardController P2H7Controller;
+    @FXML private tCardController P2H8Controller;
+    @FXML private tCardController P2H9Controller;
 
+    @FXML private tCardController P2A1Controller;
+    @FXML private tCardController P2A2Controller;
+    @FXML private tCardController P2A3Controller;
+    @FXML private tCardController P2A4Controller;
+    @FXML private tCardController P2A5Controller;
+    @FXML private tCardController P2A6Controller;
+    @FXML private tCardController P2A7Controller;
+    @FXML private tCardController P2A8Controller;
+
+    @FXML private tCardController P2S1Controller;
+    @FXML private tCardController P2S2Controller;
+    @FXML private tCardController P2S3Controller;
+    @FXML private tCardController P2S4Controller;
+    @FXML private tCardController P2S5Controller;
+    @FXML private tCardController P2S6Controller;
+    @FXML private tCardController P2S7Controller;
+    @FXML private tCardController P2S8Controller;
 
 
     @FXML private ImageView detailPict;
@@ -41,6 +88,14 @@ public class layoutController implements Initializable{
         Card.onP1H1 = AvatarDuel.landData.get(1);
         P1H2Controller.loadCard(AvatarDuel.landData.get(2));
         Card.onP1H2 = AvatarDuel.landData.get(2);
+        P1H3Controller.loadCard(AvatarDuel.landData.get(3));
+        Card.onP1H3 = AvatarDuel.landData.get(3);
+        P1H4Controller.loadCard(AvatarDuel.landData.get(4));
+        Card.onP1H4 = AvatarDuel.landData.get(4);
+        P1H5Controller.loadCard(AvatarDuel.landData.get(5));
+        Card.onP1H5 = AvatarDuel.landData.get(5);
+        P1H6Controller.loadCard(AvatarDuel.landData.get(6));
+        Card.onP1H6 = AvatarDuel.landData.get(6);
     }
 
     public void hoverP1H1(MouseEvent mouseEvent){
@@ -50,6 +105,26 @@ public class layoutController implements Initializable{
 
     public void hoverP1H2(MouseEvent mouseEvent){
         Card.cardHover = Card.onP1H2;
+        onHover();
+    }
+
+    public void hoverP1H3(MouseEvent mouseEvent){
+        Card.cardHover = Card.onP1H3;
+        onHover();
+    }
+
+    public void hoverP1H4(MouseEvent mouseEvent){
+        Card.cardHover = Card.onP1H4;
+        onHover();
+    }
+
+    public void hoverP1H5(MouseEvent mouseEvent){
+        Card.cardHover = Card.onP1H5;
+        onHover();
+    }
+
+    public void hoverP1H6(MouseEvent mouseEvent){
+        Card.cardHover = Card.onP1H6;
         onHover();
     }
 
