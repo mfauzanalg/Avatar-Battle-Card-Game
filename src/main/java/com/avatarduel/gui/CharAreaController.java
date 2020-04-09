1067bbf68afa472baccc652e72710560ffd994e3
mfauzanalg <mfauzanalg@gmail.com>
2020-04-09 22:30:18 +0700

    controller

--- src/main/java/com/avatarduel/gui/CharAreaController.java
+++ src/main/java/com/avatarduel/gui/CharAreaController.java
@@ -1,4 +1,12 @@
 package com.avatarduel.gui;
 
+import javafx.fxml.FXML;
+
 public class CharAreaController {
+    @FXML private TCardController S1Controller;
+    @FXML private TCardController S2Controller;
+    @FXML private TCardController S3Controller;
+    @FXML private TCardController S4Controller;
+    @FXML private TCardController S5Controller;
+    @FXML private TCardController S6Controller;
 }
--- src/main/java/com/avatarduel/gui/HandCardController.java
+++ src/main/java/com/avatarduel/gui/HandCardController.java
@@ -1,4 +1,17 @@
 package com.avatarduel.gui;
 
+import javafx.fxml.FXML;
+
 public class HandCardController {
+    @FXML private TCardController H1Controller;
+    @FXML private TCardController H2Controller;
+    @FXML private TCardController H3Controller;
+    @FXML private TCardController H4Controller;
+    @FXML private TCardController H5Controller;
+    @FXML private TCardController H6Controller;
+    @FXML private TCardController H7Controller;
+    @FXML private TCardController H8Controller;
+    @FXML private TCardController H9Controller;
+
+
 }
