aba30841cefd13c6403c0c0b48540d3d85f3a70f
IrfaanDzakiy <dzakiyirfaan@gmail.com>
2020-04-20 01:57:43 +0700

    add Deck docs

--- src/main/java/com/avatarduel/component/Deck.java
+++ src/main/java/com/avatarduel/component/Deck.java
@@ -8,14 +8,31 @@ public class Deck {
     public static final int MIN_DECK = 40;
     private List<Card> deck;
 
+    /**
+     * Creates a blank Deck
+     */
     public Deck(){
         this.deck  = new ArrayList<Card>();
     }
 
     //Getter Initializer Deck
+
+    /**
+     * Get the Deck List of Card
+     * @return Decks List of Card
+     */
     public List<Card> getDeck(){
         return deck;
     }
+
+    /**
+     * Initialize the Deck List of Card based on the amount of each type of card.
+     * @param nChar Character card amount
+     * @param nAura Aura skill card amount
+     * @param nLand Land card amount
+     * @param nDestroy Destroy skill card amount
+     * @param nPowerUp Power up skill card amount
+     */
     public void initializeDeck(int nChar, int nAura, int nLand, int nDestroy, int nPowerUp){
         int totalCard = nChar + nAura + nLand + nDestroy + nPowerUp;
         if ((totalCard > MAX_DECK) || (totalCard < MIN_DECK)){
@@ -36,15 +53,38 @@ public class Deck {
     }
 
     //Deck Manipulation
+
+    /**
+     * Add cards from a List of <T extends Card> to the Deck
+     * @param l List of <T>
+     * @param <T> A class that extends Card
+     */
     public <T extends Card> void addCardsToDeck(List<T> l){
         this.deck.addAll(l);
     }
+
+    /**
+     * Shuffle the Deck
+     */
     public void shuffleDeck(){
         Collections.shuffle(this.deck);
     }
+
+    /**
+     * Add a single card to the Deck
+     * @param c added card
+     * @param <T> A class that extends Card
+     */
     public <T extends Card> void addCard(T c){
         this.deck.add(c);
     }
+
+    /**
+     * Add a specific card to the Deck based on the id
+     * @param l List of <T>
+     * @param id The card's id that want to be added
+     * @param <T> A class that extends Card
+     */
     public <T extends Card> void addSpecificCard(List<T> l, int id){
         T c = l.stream()
                 .filter(i -> i.getId() == id)
@@ -54,6 +94,13 @@ public class Deck {
             this.addCard(c);
         }
     }
+
+    /**
+     * Add random cards from a List of <T> to the Deck
+     * @param l List of <T>
+     * @param quantity The amount of cards that want to be added
+     * @param <T> A class that extends Card
+     */
     public <T extends Card> void addRandomCards(List<T> l, int quantity){
         List<T> l1 = l;
         int count = 0;
